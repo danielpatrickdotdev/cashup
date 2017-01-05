77cd37f340001b2fa8fda2fa59cc42dae07f4d24
Daniel Patrick <danieljudepatrick@gmail.com>
2017-01-05 15:47:06 +0000

    Move TillClosure view URLs (use of 'closures/' path no longer seems necessary)

--- cashup/urls.py
+++ cashup/urls.py
@@ -4,13 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.TillClosureListView.as_view(),
-        name='cashup_till_closure_list'),
-    url(r'^closures/(?P<pk>[0-9]+)/$', views.TillClosureDetailView.as_view(),
+        name='cashup_closures_list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.TillClosureDetailView.as_view(),
         name='cashup_closure_detail'),
-    url(r'^closures/$', views.TillClosureListView.as_view(),
-        name='cashup_closure_list'),
-    url(r'^closures/(?P<pk>[0-9]+)/edit/$', views.TillClosureUpdateView.as_view(),
+    url(r'^(?P<pk>[0-9]+)/edit/$', views.TillClosureUpdateView.as_view(),
         name='cashup_closure_update'),
-    url(r'^closures/create/$', views.TillClosureCreateView.as_view(),
+    url(r'^create/$', views.TillClosureCreateView.as_view(),
         name='cashup_closure_create'),
 ]
