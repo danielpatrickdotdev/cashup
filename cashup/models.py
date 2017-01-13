cbdcd81ea2f05206678bb54797563a6581511f08
remarkablerocket <danny@justaboutanywhere.co.uk>
2017-01-13 12:04:11 +0000

    Change Business name to CharField as we don't appear to be making use of SlugField features anyway or plan to at this point

--- cashup/models.py
+++ cashup/models.py
@@ -37,7 +37,7 @@ class Personnel(models.Model):
 
 
 class Business(models.Model):
-    name = models.SlugField(max_length=12)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
