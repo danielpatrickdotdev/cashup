79ee077e91c7d0117801ad9d0802d7d67b198447
remarkablerocket <danny@justaboutanywhere.co.uk>
2017-01-17 13:01:56 +0000

    Make more modular and hopefully more clear what's going on so easy to modify later

--- cashup/managers.py
+++ cashup/managers.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 from django.db import models
 from django.db.models import Q
 
@@ -9,8 +12,22 @@ class AuditTrailManager(models.Manager):
 
 class OutletQuerySet(models.QuerySet):
     def for_personnel(self, personnel, is_manager=False):
+        """
+        Gets all Outlets which personnel object is associated with.
+        For business owners this always includes all the Outlets in their
+        business.
+        Optional `is_manager` determines wether (for non-owners) the returned
+        queryset will include Outlets they are staff of or just ones they
+        manage.
+        """
+        filters = [
+            (Q(business__personnel=personnel) &
+             Q(business__personnel__is_owner=True)),
+        ]
         if is_manager:
-            return self.filter(staff__is_manager=True,
-                               staff__personnel=personnel)
+            filters.append((Q(staff__is_manager=True) &
+                            Q(staff__personnel=personnel)))
         else:
-            return self.filter(staff__personnel=personnel)
+            filters.append(Q(staff__personnel=personnel))
+        q_obj = reduce(operator.or_, filters)
+        return self.filter(q_obj).distinct().order_by('name')
